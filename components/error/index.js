9afce285a35626f3a25ac65d7dd373bebf0d0542
Gonçalo Morais <gnclmorais@gmail.com>
2020-07-20 22:13:59 +0100

    Improve suggest term link (#271)
    * Add missing search term to suggestion link
    
    Having a GitHub link to directly suggest a term is great, and it
    could be even better if we could pass on the term the user already
    typed onto the address bar. Link magic!
    
    * Highlight the missing term
    
    Now that we have access to it, we could show it on the page and
    highlight it a bit. Otherwise, it is only on the address bar. Added
    “yet” as well as a small hint that this glossary grows and people’s
    suggestions are valuable.
    
    * Cleanup GitHub’s new issue link
    
    A couple of parameters we were passing were not being used (no
    content present), so we can provide a simpler user experience by
    removing them for the time being.

--- components/error/index.js
+++ components/error/index.js
@@ -4,7 +4,7 @@ import Page from '@components/page'
 import Link from '@components/link'
 import styles from './error.module.css'
 
-const Error = ({ status }) => {
+const Error = ({ missingTerm, status }) => {
   return (
     <Page title={status || 'Error'} showSlug={false}>
       <Head>
@@ -15,10 +15,10 @@ const Error = ({ status }) => {
         <>
           <h1>404</h1>
           <p>
-            The term you are looking for doesn't exist. You can{' '}
+            The term <strong>{missingTerm}</strong> doesn’t exist yet. You can{' '}
             <Link
               underline
-              href="https://github.com/gaearon/whatthefuck.is/issues/new?assignees=&labels=&template=what-the-fuck-is-_____-.md&title="
+              href={ `https://github.com/gaearon/whatthefuck.is/issues/new?template=what-the-fuck-is-_____-.md&title=WTF is ${missingTerm}?` }
             >
               suggest this term
             </Link>
--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,21 @@
+import { withRouter } from 'next/router'
+
 import Error from '@components/error'
 
-export default function E() {
-  return <Error status={404} />
+// Included just to prevent Automatic Static Optimization
+export async function getStaticProps(context) {
+  return {
+    props: {}
+  }
 }
+
+// Strips potential `/term?something=else` into `term`
+const extractTerm = path => path.substring(
+  path.indexOf('/') + 1, Math.max(path.indexOf('?'), path.length)
+)
+
+function E({ router }) {
+  return <Error missingTerm={extractTerm(router.asPath)} status={404} />
+}
+
+export default withRouter(E)
